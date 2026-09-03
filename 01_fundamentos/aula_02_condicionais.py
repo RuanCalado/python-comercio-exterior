6ec5056783b1f6f51873cb3631c6c70c86f0d9f0
Ruan Calado <294666129+RuanCalado@users.noreply.github.com>
2026-09-03 14:46:56 -0300

    Adicionando exercício de condicionais

---
+++ 01_fundamentos/aula_02_condicionais.py
@@ -0,0 +1,9 @@
+# Exercício: validação simples
+
+quantidade_invoice = 100
+quantidade_pl = 100
+
+if quantidade_invoice == quantidade_pl:
+    print("OK: as quantidades conferem.")
+else:
+    print("ALERTA: as quantidades divergem.")
