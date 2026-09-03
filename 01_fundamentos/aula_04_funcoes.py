d84e1cdebe4e2520bd40ec956df8f8f86932c974
Ruan Calado <294666129+RuanCalado@users.noreply.github.com>
2026-09-03 14:47:07 -0300

    Adicionando exercício de funções

---
+++ 01_fundamentos/aula_04_funcoes.py
@@ -0,0 +1,8 @@
+# Exercicio de funcao
+
+def calcular_peso_total(quantidade, peso_unitario):
+    return quantidade * peso_unitario
+
+
+peso = calcular_peso_total(20, 1.8)
+print(f"Peso total: {peso} kg")
