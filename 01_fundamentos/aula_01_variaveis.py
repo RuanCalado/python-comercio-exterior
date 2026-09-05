09d9bd45c9b0e66b98595024867a2b4197992346
Ruan Calado <ruanrecebimentos@outlook.com>
2026-09-05 12:34:08 -0300

    Criando calculadora inicial de peso

--- 01_fundamentos/aula_01_variaveis.py
+++ 01_fundamentos/aula_01_variaveis.py
@@ -1,11 +1,12 @@
 # Primeiro exercício: variáveis e cálculo de peso
 
-produto = "Produto Exemplo"
-quantidade = 10
-peso_unitario_kg = 2.5
+produto = "Pneu 14.00-24"
+quantidade = 10 
+peso_unitario = 82.5
 
-total_peso_kg = quantidade * peso_unitario_kg
+peso_total = quantidade * peso_unitario
 
-print(f"Produto: {produto}")
-print(f"Quantidade: {quantidade}")
-print(f"Peso total: {total_peso_kg} kg")
+print("Produto:", produto)
+print("Quantidade:", quantidade)
+print("Peso unitário:", peso_unitario, "kg")
+print("Peso Total:", peso_total, "kg")
